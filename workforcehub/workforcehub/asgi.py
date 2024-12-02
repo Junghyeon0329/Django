9017baaba9becf5d271296121a080b18a6eb6149
Junghyeon0329 <sptlsrja@gmail.com>
2024-12-02 22:19:10 +0900

    회사 시스템 설계 수정

--- workforcehub/workforcehub/asgi.py
+++ workforcehub/workforcehub/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for workforce project.
+ASGI config for workforcehub project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workforce.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workforcehub.settings')
 
 application = get_asgi_application()
--- workforcehub/workforcehub/wsgi.py
+++ workforcehub/workforcehub/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for workforce project.
+WSGI config for workforcehub project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workforce.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'workforcehub.settings')
 
 application = get_wsgi_application()
