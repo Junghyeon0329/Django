0bae73dce16101e71c82c2095465fc0bb9ad3012
Junghyeon0329 <sptlsrja@gmail.com>
2024-12-05 21:06:13 +0900

    모든 유저 정보 조회

--- operationhub/operation_action/views.py
+++ operationhub/operation_action/views.py
@@ -7,11 +7,14 @@ import os
 
 class UserAPIView(views.APIView):
 	# 외부 API에서 유저 정보를 가져오는 함수
-	def fetch_user_info(self, email_id):
+	def fetch_user_info(self, email_id=None):
 		try:
 			sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))       
 			from URLaddress import workforceURL
-			res = requests.get(f"http://{workforceURL['ip']}:{workforceURL['port']}/users/?email_id={email_id}")
+			if email_id:
+				res = requests.get(f"http://{workforceURL['ip']}:{workforceURL['port']}/users/?email_id={email_id}")
+			else :
+				res = requests.get(f"http://{workforceURL['ip']}:{workforceURL['port']}/users/")
 			res.raise_for_status()                
 			return res.json().get("data", {})
 		except requests.exceptions.RequestException as e:
@@ -20,13 +23,12 @@ class UserAPIView(views.APIView):
 	def get(self, request, *args, **kwargs):
 		# 쿼리 파라미터에서 이메일 ID를 가져옵니다.
 		email_id = request.query_params.get('email_id', None)
-		
-		# 외부 API로부터 유저 정보를 가져오기
-		if email_id:
-			user_info = self.fetch_user_info(email_id)
-			if user_info:
-				return response.Response({"success": True, "data": user_info})
-			else:
+  
+		user_info = self.fetch_user_info(email_id)
+		if user_info:
+			return response.Response({"success": True, "data": user_info})
+		else:
+			if email_id:
 				return response.Response({"success": False, "message": "User not found"}, status=404)
-
-		return response.Response({"success": True, "data": []})
+			else:
+				return response.Response({"success": True, "data": []})
