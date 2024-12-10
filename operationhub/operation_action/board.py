f27cae9ad4e7105e536e4f5354abf9d9ae99d35a
Junghyeon0329 <sptlsrja@gmail.com>
2024-12-10 20:53:21 +0900

    관리자 특정 유저 검색 및 user_id 출력

--- operationhub/operation_action/board.py
+++ operationhub/operation_action/board.py
@@ -4,20 +4,29 @@ from .serializers import BoardSerializer
 from rest_framework.permissions import IsAuthenticated
 
 class BoardAPIView(views.APIView):
-    
-    permission_classes = [IsAuthenticated]  # 인증된 사용자만 접근 가능
-    
-    # GET 요청: 게시글 목록 가져오기
-    def get(self, request, *args, **kwargs):
-        boards = Board.objects.all().order_by('-created_at')
-        serializer = BoardSerializer(boards, many=True)  # 여러 개의 게시글을 직렬화
-        return response.Response(serializer.data)  # 직렬화된 데이터 반환
+	
+	permissions = [IsAuthenticated]
+	
+	# GET 요청: 게시글 목록 가져오기
+	def get(self, request, *args, **kwargs):
+		if request.user.is_superuser or request.user.is_staff:
+			user_id = request.query_params.get('user_id', None)
+			if user_id:
+				boards = Board.objects.filter(author_id=user_id).order_by('-created_at')
+			else:
+				boards = Board.objects.all().order_by('-created_at')            
+		else:
+			# 일반 사용자는 자신의 게시글만 반환
+			boards = Board.objects.filter(author=request.user).order_by('-created_at')
+		
+		serializer = BoardSerializer(boards, many=True)  # 여러 개의 게시글을 직렬화
+		return response.Response(serializer.data)  # 직렬화된 데이터 반환
 
-    # POST 요청: 게시글 생성
-    def post(self, request, *args, **kwargs):
-        serializer = BoardSerializer(data=request.data, context={'request': request})
-        if serializer.is_valid():     
-            serializer.save()
-            return response.Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+	# POST 요청: 게시글 생성
+	def post(self, request, *args, **kwargs):
+		serializer = BoardSerializer(data=request.data, context={'request': request})
+		if serializer.is_valid():     
+			serializer.save()
+			return response.Response(serializer.data, status=status.HTTP_201_CREATED)
+		
+		return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
--- operationhub/operation_action/serializers.py
+++ operationhub/operation_action/serializers.py
@@ -5,8 +5,9 @@ from .models import Board
 class BoardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Board
-        fields = ['id', 'title', 'content', 'created_at', 'updated_at']  # 'author' 제외
+        fields = ['id', 'title', 'content', 'author_id', 'created_at', 'updated_at']
 
     def create(self, validated_data):
-        validated_data['author'] = self.context['request'].user  # 로그인한 사용자 설정
+        # POST 요청 시, 클라이언트가 author를 제공할 필요 없이 자동으로 로그인한 사용자 설정
+        validated_data['author'] = self.context['request'].user
         return super().create(validated_data)
