efa31b857c5cfe9dfd0d19a4815bceae6f3e7d25
Junghyeon0329 <sptlsrja@gmail.com>
2024-12-13 21:36:35 +0900

    게시판 파일 업 기능 구현 설계

--- operationhub/operation_action/models.py
+++ operationhub/operation_action/models.py
@@ -14,3 +14,16 @@ class Board(models.Model):
         
     def __str__(self):
         return self.title
+
+class File(models.Model):
+    board = models.ForeignKey(Board, related_name='files', on_delete=models.CASCADE)  # 게시글과 관계
+    file = models.FileField(upload_to='uploads/%Y/%m/%d/')  # 파일 저장 경로
+    name = models.CharField(max_length=255)  # 파일 이름
+    file_size = models.PositiveIntegerField()  # 파일 크기 (바이트 단위)
+    uploaded_at = models.DateTimeField(auto_now_add=True)  # 파일 업로드 일시
+    
+    class Meta:
+        db_table = "board_files"
+    
+    def __str__(self):
+        return self.name
