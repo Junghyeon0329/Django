4cad337c5977c18ef5a8fa5d910ca4d2ed3ddda3
Junghyeon0329 <sptlsrja@gmail.com>
2024-12-16 09:25:34 +0900

    user 정보 가입 및 조회 수정

--- operationhub/operation_action/board.py
+++ operationhub/operation_action/board.py
@@ -4,36 +4,10 @@ from .serializers import BoardSerializer
 
 class BoardAPIView(views.APIView):
     permission_classes = [permissions.IsAuthenticated]
-    
-    def get(self, request, *args, **kwargs):
-        """ GET 요청: 게시글 목록 가져오기 """
-        user_id = request.query_params.get('user_id', None)
-        
-        # 관리자일 경우, 모든 게시글을 가져올 수 있음
-        if request.user.is_staff:
-            boards = Board.objects.filter(user_id=user_id) if user_id else Board.objects.all()
-        else:
-            # 일반 사용자는 본인 게시글만 볼 수 있음
-            boards = Board.objects.filter(user=request.user)
-            if user_id and user_id != str(request.user.id):
-                return response.Response(
-                    {"success": False, "message": "You are not authorized to view this user's posts."},
-                    status=status.HTTP_403_FORBIDDEN
-                )
-
-        # 게시글이 없으면 오류 응답 반환
-        if not boards.exists():
-            return response.Response(
-                {"success": False, "message": "No posts found."},
-                status=status.HTTP_404_NOT_FOUND
-            )
-
-        # 게시글을 직렬화하고 반환
-        serializer = BoardSerializer(boards, many=True)
-        return response.Response(serializer.data)
-    
+     
+    """ POST 요청: 게시글 생성 """
     def post(self, request, *args, **kwargs):
-        """ POST 요청: 게시글 생성 """
+       
         serializer = BoardSerializer(data=request.data, context={'request': request})
         
         if serializer.is_valid():
@@ -42,9 +16,9 @@ class BoardAPIView(views.APIView):
         
         return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    
+    """ DELETE 요청: 게시글 삭제 """ 
     def delete(self, request, *args, **kwargs):
-        """ DELETE 요청: 게시글 삭제 """
+       
         board_id = request.query_params.get('board_id')
 
         # board_id가 없으면 400 오류 반환
@@ -76,3 +50,32 @@ class BoardAPIView(views.APIView):
             {"success": False, "message": "You are not authorized to delete this post."},
             status=status.HTTP_403_FORBIDDEN
         )
+        
+    """ GET 요청: 게시글 목록 가져오기 """
+    def get(self, request, *args, **kwargs):
+        
+        user_id = request.query_params.get('user_id', None)
+        
+        # 관리자일 경우, 모든 게시글을 가져올 수 있음
+        if request.user.is_staff:
+            boards = Board.objects.filter(user_id=user_id) if user_id else Board.objects.all()
+        else:
+            # 일반 사용자는 본인 게시글만 볼 수 있음
+            boards = Board.objects.filter(user=request.user)
+            if user_id and user_id != str(request.user.id):
+                return response.Response(
+                    {"success": False, "message": "You are not authorized to view this user's posts."},
+                    status=status.HTTP_403_FORBIDDEN
+                )
+
+        # 게시글이 없으면 오류 응답 반환
+        if not boards.exists():
+            return response.Response(
+                {"success": False, "message": "No posts found."},
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+        # 게시글을 직렬화하고 반환
+        serializer = BoardSerializer(boards, many=True)
+        return response.Response(serializer.data)
+   
--- operationhub/operationhub/urls.py
+++ operationhub/operationhub/urls.py
@@ -1,37 +1,22 @@
-"""
-URL configuration for operationhub project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
-
-from operation_action.views import UserAPIView
-from operation_action.workforce import WorkforceAPIView
-from operation_action.board import BoardAPIView
-from operation_action.logins import LoginAPIView
+from django.contrib import admin
 from rest_framework_simplejwt import views as jwt_views
+
+from operation_action.manage import UserAPIView, LoginAPIView
+from operation_action.board import BoardAPIView
+from operation_action.workforce import WorkforceAPIView
 
 
 ''' 관리자를 추가하는 방법 : python manage.py createsuperuser '''
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('info/', UserAPIView.as_view(), name='info-api'),
-    path('users/', WorkforceAPIView.as_view(), name='user-api'),
-    path('board/', BoardAPIView.as_view(), name='board-api'),
-    path('login/', LoginAPIView.as_view(), name='login-api'),    
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh')
+    # path('admin/', admin.site.urls),
+    # path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    
+    path('login/', LoginAPIView.as_view(), name='login-api'),
+    path('user/', UserAPIView.as_view(), name='user-api'),    
+    path('board/', BoardAPIView.as_view(), name='board-api'),    
+    
+    # path('users/', WorkforceAPIView.as_view(), name='user-api'),
 ]
     
