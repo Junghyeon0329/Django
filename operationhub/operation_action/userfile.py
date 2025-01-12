6f6aeb864cefcaf0f9828e6fd901bc41a1de67b5
Junghyeon0329 <sptlsrja@gmail.com>
2025-01-12 19:53:34 +0900

    다운로드 파일 완료

--- operationhub/operation_action/userfile.py
+++ operationhub/operation_action/userfile.py
@@ -19,21 +19,21 @@ class FileAPIView(APIView):
 			
 			if not file_type:
 				return response.Response(
-					{"error": "fileType parameter is required"},
+					{"success": False, "message": "fileType parameter is required"},
 					status=status.HTTP_400_BAD_REQUEST
 				)
     
-			# file_path = os.path.join('files/', f"{file_type}.pdf")
-			file_path = os.path.join(settings.MEDIA_ROOT, 'files', "testtest.csv")
+	
+			file_path = os.path.join(settings.MEDIA_ROOT, 'files', f"{file_type}.docx")
 			if not os.path.exists(file_path):
 				return response.Response(
-					{"error": "File not found"},
-					status=status.HTTP_404_NOT_FOUND
+					{"success": False, "message": "File not found"},
+					status=status.HTTP_400_BAD_REQUEST
 				)
-			return FileResponse(open(file_path, 'rb'), content_type='application/csv')   
+			return FileResponse(open(file_path, 'rb'), content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
 
 		except Exception as e:
 			return response.Response(
-				{"error": str(e)},
-				status=status.HTTP_500_INTERNAL_SERVER_ERROR
+					{"success": False, "message": str(e)},
+					status=status.HTTP_400_BAD_REQUEST
 			)
